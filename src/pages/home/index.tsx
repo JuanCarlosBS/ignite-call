3393ac66ea94c75c11fcc51b58d2e853a7aaa74d
Juan Carlos <juanrov_@hotmail.com>
2023-02-12 02:08:52 -0300

    chore: config next project

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,17 @@
+import { Heading, Text } from '@ignite-ui/react'
+import { Container, Hero, Preview } from './styles'
+
+export default function Home() {
+  return (
+    <Container>
+      <Hero>
+        <Heading size="4xl">Agendamento descomplicado.</Heading>
+        <Text size="lg">
+          Conete seu calendário e permita que as pessoas marquem agendamentos no
+          seu tempo livre
+        </Text>
+      </Hero>
+      <Preview></Preview>
+    </Container>
+  )
+}
